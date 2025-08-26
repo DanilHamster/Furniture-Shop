09c0be15980608ae0df7fea1b482bcf1922377ac
Hamster <dtaranenko137@gmail.com>
2025-08-26 18:51:02 +0300

    proces fixing

--- Market/settings/prod.py
+++ Market/settings/prod.py
@@ -1,8 +1,8 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["hamster-shop.onrender.com", "127.0.0.1", "localhost"]
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
